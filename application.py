a3af0b15f8a2c6b4a0daa673fdc1814696f9e276
Jack Allen <Allenj25@nku.edu>
2020-02-15 16:25:16 -0500

    Connecting to db

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
 from flask import Flask
-
+from repos.classes import Customer, Agents, Loan, Transaction, Accounts, Card
+from repos.db_conn import test
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Loren ipsum dolor sit amet"
+
+@app.route("/test", methods=['GET'])
+def testtwo():
+    return test
 
--- repos/db_conn.py
+++ repos/db_conn.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import sessionmaker
 import sqlalchemy
 
 db_url = url.URL(
-    drivername='py',
-    username='',
-    password='',
-    host='127.0.0.1',
-    port='3306',
-    database='',
+    drivername='ODBC Driver 13 for SQL Server',
+    username='allenj25',
+    password='Jack1998',
+    host='css439.database.windows.net',
+    port='1433',
+    database='datawarehouse',
 )
-
+test= "please for the love of god work"
 engine = create_engine(db_url)
 
 Session = sessionmaker(bind = engine)
