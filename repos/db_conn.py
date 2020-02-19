31bc82e4c0a89d69f9dee8ffc1e11215b35a7127
Farrow1 <Allenj25@nku.edu>
2020-02-18 20:24:05 -0500

    Update db_conn.py

--- repos/db_conn.py
+++ repos/db_conn.py
@@ -14,7 +14,7 @@ server='tcp:csc439.database.windows.net'
 database='datawarehouse'
 username = 'allenj25'
 password = 'Jack1998'
-driver = '{ODBC Driver 17 for SQL Server}'
+driver = '{ODBC Driver 13 for SQL Server}'
 cnxn = ('DRIVER= ' + driver + ';SERVER='+server+',1433;DATABASE='+database+';UID='+username+';PWD='+password)
 
 connect_str = 'mssql+pyodbc:///?odbc_connect=' + urllib.parse.quote_plus(cnxn) + "?driver=SQL+Server"
