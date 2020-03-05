f572d2aa2a1dac665421b4d25984f7aafb94c372
Jack <Allen25@nku.edu>
2020-03-05 17:05:56 -0500

    added repo functions

--- application.py
+++ application.py
@@ -2,8 +2,6 @@ from flask import Flask,jsonify
 from repos.classes import Customer, Agents, Loan, Transaction, Accounts, Card
 from repos.db_conn import session
 
-
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,6 +13,37 @@ def customers(id):
     customer = session.query(Customer).get(id)
 
     return jsonify([customer.customer_Id, customer.first_name, customer.last_name, customer.phone, customer.address, customer.agent_id])
+
+@app.route("/api/account/<id>")
+def accounts(id):
+    account = session.query(Accounts).get(id)
+
+    return jsonify([account.accountID, account.customerID, account.balance, account.interest, account.isIvestment, account.pin])
+
+@app.route("/api/transaction/<id>")
+def transaction(id):
+    transaction = session.query(Transaction).get(id)
+
+    return jsonify([transaction.transactionID, transaction.date, transaction.amount, transaction.source, transaction.destination])
+
+@app.route("/api/card/<number>")
+def card(number):
+    card = session.query(Card).get(number)
+
+    return jsonify([card.cardNumber, card.accountID, card.cardType])
+
+@app.route("/api/loan/<id>")
+def loan(id):
+    loan = session.query(Loan).get(id)
+
+    return jsonify([loan.loanId, loan.accountId, loan.amount, loan.interest])
+
+@app.route("/api/agents/<id>")
+def agents(id):
+    agents = session.query(Agents).get(id)
+
+    return jsonify([agents.agentId, agents.firstName, agents.lastName])
+
 
 @app.errorhandler(500)
 def key_not_found(e):
